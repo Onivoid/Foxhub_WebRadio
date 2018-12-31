354c01a10cac0eaebb2103fa26a56833d46c01a2
Logan Wilquin <lwilquin@datasolution.fr>
2018-12-31 12:20:34 +0100

    Home Page Update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ class App extends Component {
         return (
           <BrowserRouter>
             <div>
-              <Layout/>
               <Route path="/" exact="true" component={Home} />
             </div>
           </BrowserRouter>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import Logo from './images/SkullyfoxPoly.png';
 import './Home.sass';
 
 class Home extends Component {
@@ -9,13 +10,22 @@ class Home extends Component {
   render(){
     return(
       <div class="Home">
-        <div class="Home_title">
-          <p class="title">Bienvenu sur le site</p>
-          <p class="title">de la communautée Foxhub</p>
+        <div class="left">
+
         </div>
-        <div class="Home_button">
-          <a href="" class="button">Visiter en tant qu'invité</a>
-          <a href="" class="button">M'authentifier</a>
+        <div class="right">
+          <img src={Logo} class="logo"/>
+          <p class="title">Foxhub Community</p>
+          <p class="subtitle">Site communautaire Foxhub</p>
+          <div class="login">
+            <p class="subtitle">Connexion</p>
+            <input type="text" class="input" placeholder="Pseudo"/>
+            <input type="password" class="input" placeholder="Mot de Passe"/>
+            <div class="links">
+              <a href="" class="sign-button">Se connecter</a>
+              <a href="" class="sign-button">S'inscrire</a>
+            </div>
+          </div>
         </div>
       </div>
     );
